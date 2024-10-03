4001a17a4d463bdac78acde14a68a896b1f4742b
Vaibhav Jain <vaibhavjain8880@gmail.com>
2024-10-03 16:32:56 +0530

    second commit

--- src/main/java/com/vaibhav/jain/DemoProjectApplication.java
+++ src/main/java/com/vaibhav/jain/DemoProjectApplication.java
@@ -3,12 +3,17 @@ package com.vaibhav.jain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vaibhav.jain.util.Sample;
+
 @SpringBootApplication
 public class DemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
 		System.out.println("Vaibhav Jain");
+		
+		Sample sample = new Sample();
+		sample.m1();
 	}
 
 }
---
+++ src/main/java/com/vaibhav/jain/util/Sample.java
@@ -0,0 +1,12 @@
+package com.vaibhav.jain.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+	
+  public	void m1() {
+		System.out.println("Running");
+	}
+
+}
